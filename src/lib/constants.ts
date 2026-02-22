5385eb60e050d0506b4176f5e3e711c5ac9439c3
Aakash Dev <100058578+aakash07l@users.noreply.github.com>
2026-02-22 12:33:36 +0530

    Update SUPPORTED_CHAIN to use defineChain for BSC

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
-import { binance } from "thirdweb/chains";
+import { defineChain } from "thirdweb";
 
-export const SUPPORTED_CHAIN = binance;
+export const SUPPORTED_CHAIN = defineChain(56); // Binance Smart Chain Mainnet
 export const USDT_CONTRACT_ADDRESS = "0x55d398326f99059fF775485246999027B3197955";
 export const DEFAULT_USDT_INR_RATE = 90.0;
 export const ADMIN_WALLET_ADDRESS = "0x0000000000000000000000000000000000000000";
